a731d77e5694df0c0d2bb5489d61850f444d7eec
SmoothCrimminal <73589080+SmoothCrimminal@users.noreply.github.com>
2021-01-16 12:47:07 +0100

    Add Client

---
+++ Messenger/Messenger/FrmSend.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Messenger
+{
+    partial class FrmSend
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessages = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtMessageBox = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtMessages
+            // 
+            this.txtMessages.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtMessages.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtMessages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtMessages.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtMessages.Location = new System.Drawing.Point(0, 0);
+            this.txtMessages.Multiline = true;
+            this.txtMessages.Name = "txtMessages";
+            this.txtMessages.ReadOnly = true;
+            this.txtMessages.Size = new System.Drawing.Size(643, 606);
+            this.txtMessages.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtMessageBox);
+            this.panel1.Controls.Add(this.btnSend);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 506);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(643, 100);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtMessageBox
+            // 
+            this.txtMessageBox.Location = new System.Drawing.Point(12, 8);
+            this.txtMessageBox.Multiline = true;
+            this.txtMessageBox.Name = "txtMessageBox";
+            this.txtMessageBox.Size = new System.Drawing.Size(408, 80);
+            this.txtMessageBox.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSend.Location = new System.Drawing.Point(438, 8);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(176, 80);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // FrmSend
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(643, 606);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.txtMessages);
+            this.Name = "FrmSend";
+            this.Text = "MessageBox";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessages;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtMessageBox;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
---
+++ Messenger/Messenger/FrmSend.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessengerLogic;
+
+namespace Messenger
+{
+    public partial class FrmSend : Form
+    {
+        public static string _from, _msg;
+        public FrmSend()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string to = FrmSend.ActiveForm.Text;
+            if (txtMessageBox.Text.Trim() == "")
+            {
+
+            }
+
+            else
+            {
+                string message = txtMessageBox.Text;
+                Connection sendMessage = new Connection();
+                sendMessage.SendMessage(to, message);
+                txtMessageBox.Text = "";
+                txtMessages.AppendText($"You: {message}");
+                txtMessages.AppendText(Environment.NewLine);
+            }
+        }
+
+
+        public void MessageFromClient()
+        {
+
+            txtMessages.Invoke(new Action(delegate ()
+            {
+                txtMessages.AppendText($"{_from}: {_msg}");
+                txtMessages.AppendText(Environment.NewLine);
+            }));
+        }
+
+        public void AssignValues(string from, string msg)
+        {
+            _from = from;
+            _msg = msg;
+        }
+    }
+}
